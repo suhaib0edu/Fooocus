31d6caae417495d797b1def994483427193f958c
Suhaib Eltayeb <99388048+suhaib0edu@users.noreply.github.com>
2024-05-15 15:14:27 +0200

    Update webui.py
    None image

--- webui.py
+++ webui.py
@@ -63,7 +63,7 @@ def generate_clicked(task: worker.AsyncTask):
 
                 percentage, title, image = product
                 yield gr.update(visible=True, value=modules.html.make_progress_html(percentage, title)), \
-                    gr.update(visible=True, value=image) if image is not None else gr.update(), \
+                    gr.update(visible=True, value=image) if None is not None else gr.update(), \
                     gr.update(), \
                     gr.update(visible=False)
             if flag == 'results':
